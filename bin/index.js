c2c7e3c04f41666c90feef96db6bb7f1250888e2
afeiship <1290657123@qq.com>
2024-01-26 18:01:12 +0800

    feat: init basic logic

--- bin/index.js
+++ bin/index.js
@@ -3,27 +3,55 @@
 import { Command } from 'commander';
 import { join } from 'path';
 import { loadJsonFileSync } from 'load-json-file';
+import fg from 'fast-glob';
+import replaceInFile from 'replace-in-file';
+
+import '@jswork/next-yaml-configuration';
 
 const __dirname = new URL('../', import.meta.url).pathname;
 const pkg = loadJsonFileSync(join(__dirname, 'package.json'));
 const program = new Command();
 
 program.version(pkg.version);
-program.option('-f, --force', 'force to create').parse(process.argv);
+program.option('-c, --config', 'Config file path.', 'config.yaml').parse(process.argv);
 
 /**
  * @help: rifc -h
- * @description: rifc -f
+ * @description: rifc -c
  */
 
 class CliApp {
   constructor() {
     this.args = program.args;
     this.opts = program.opts();
+    this.cfg = new nx.YamlConfiguration({
+      path: join(__dirname, 'config.yaml'),
+    });
+  }
+
+  getFiles() {
+    const globs = this.cfg.get('files') || '*';
+    return fg.glob.sync(globs);
+  }
+
+  getReplacements() {
+    return this.cfg.get('replacements');
   }
 
   run() {
-    console.log('run cli: ', __dirname, this.args, this.opts);
+    // 1. get files
+    const files = this.getFiles();
+    // 2. replace by replacement
+    const replacements = this.getReplacements();
+
+    // 3. do replace
+    replacements.forEach((replacement) => {
+      console.log(files, replacement);
+      // replaceInFile({
+      //   files,
+      //   ...replacement,
+      // });
+    });
   }
 }
 
