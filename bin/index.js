46b7fc07dcbcbebac72e37fa9009cd5342d77cc3
afeiship <1290657123@qq.com>
2024-01-26 18:15:09 +0800

    feat: add cmd init/replace

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,7 @@ import { Command } from 'commander';
 import { join } from 'path';
 import { loadJsonFileSync } from 'load-json-file';
 import fg from 'fast-glob';
+import fs from 'node:fs';
 import replaceInFile from 'replace-in-file';
 
 import '@jswork/next-yaml-configuration';
@@ -11,9 +12,14 @@ import '@jswork/next-yaml-configuration';
 const __dirname = new URL('../', import.meta.url).pathname;
 const pkg = loadJsonFileSync(join(__dirname, 'package.json'));
 const program = new Command();
+const initConfigFile = join(__dirname, 'config.init.yaml');
 
 program.version(pkg.version);
-program.option('-c, --config', 'Config file path.', 'config.yaml').parse(process.argv);
+program
+  .option('-i, --init', 'Init config file.', false)
+  .option('-c, --config', 'Config file path.', 'config.yaml')
+  .option('-v, --verbose', 'Verbose mode.', false)
+  .parse(process.argv);
 
 /**
  * @help: rifc -h
@@ -25,7 +31,7 @@ class CliApp {
     this.args = program.args;
     this.opts = program.opts();
     this.cfg = new nx.YamlConfiguration({
-      path: join(__dirname, 'config.yaml'),
+      path: join(process.cwd(), this.opts.config),
     });
   }
 
@@ -38,20 +44,34 @@ class CliApp {
     return this.cfg.get('replacements');
   }
 
-  run() {
+  cmdReplace() {
+    const { verbose } = this.opts;
     // 1. get files
     const files = this.getFiles();
     // 2. replace by replacement
     const replacements = this.getReplacements();
 
     // 3. do replace
-    replacements.forEach((replacement) => {
-      console.log(files, replacement);
-      // replaceInFile({
-      //   files,
-      //   ...replacement,
-      // });
-    });
+    for (let replacement of replacements) {
+      replaceInFile({
+        files,
+        ...replacement,
+      }).then((results) => {
+        if (verbose) console.log('⚡️Replacement results:', results);
+      });
+    }
+  }
+
+  cmdInit() {
+    const { init, verbose } = this.opts;
+    if (!init) return;
+    fs.copyFileSync(initConfigFile, this.opts.config);
+    if (verbose) console.log('⚡️Init config file:', this.opts.config);
+  }
+
+  run() {
+    this.cmdInit();
+    this.cmdReplace();
   }
 }
 
